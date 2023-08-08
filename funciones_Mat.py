e04b25ef265623b422986703148d0e134ed8fd9d
Ronny Matute <rgmatute91@gmail.com>
2023-08-07 20:13:16 -0500

    Agregue funcion para restar 2 numeros

--- funciones_Mat.py
+++ funciones_Mat.py
@@ -3,3 +3,8 @@ def suma_simple(n1, n2):
     return n1 + n2
 
 print ("La suma es: {}".format(suma_simple(25, 10)))
+
+def resta_simple(n1, n2):
+    return n1-n2
+
+print("La resta es: {}".format(resta_simple(9,2)))
